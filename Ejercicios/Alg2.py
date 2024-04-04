6d321a6bf27bebf08d76d2e706f08c9d92640d0f
Mike <michael16cd@gmail.com>
2024-04-04 10:18:49 -0500

    Nueno ejercicio

---
+++ Ejercicios/Alg2.py
@@ -0,0 +1,12 @@
+'''Algoritmo para encontrar la sucesión de Fibonacci hasta un No. Dado.'''
+
+
+print('Secuencia de Fibonacci')
+n = int(input('Hasta que número quiere que llegue la secuencia: '))
+actual = 1
+nuevo = 0
+for i in range(n):
+    nuevo = actual + nuevo
+    actual = nuevo - actual
+    print(nuevo)
+
